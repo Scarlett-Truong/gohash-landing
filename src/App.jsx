0fcfe5dad715b03f1d67b92d6cd49f01a897fb78
RyoDMG <ryo@dmgblockchain.com>
2022-04-14 00:49:06 -0700

    commented out unnessesary components

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Helmet } from "react-helmet";
 import { Switch, Route } from "react-router-dom";
 import Aos from "aos";
 
-import Home from "./pages/Home";
-import HomeDemo1 from "./template/HomeDemo1";
-import HomeDemo2 from "./template/HomeDemo2";
+// import Home from "./pages/Home";
+// import HomeDemo1 from "./template/HomeDemo1";
+// import HomeDemo2 from "./template/HomeDemo2";
 import HomeDemo3 from "./template/HomeDemo3";
 
 import "aos/dist/aos.css";
@@ -30,17 +30,7 @@ const App = () => {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <title>NFT Marketplace</title>
-        <link
-          href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
-          rel="stylesheet"
-        />
-        <link
-          rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
-          integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0="
-          crossorigin="anonymous"
-        />
+        <title>GoHash</title>
       </Helmet>
       <Switch>
         <Route path="/" exact component={HomeDemo3} />
